091435a5a9e998c4fcd03a6b297addc2201c7c10
Xunnhu <101741985+Xunnhu@users.noreply.github.com>
2022-03-17 01:16:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,33 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+   		    introTitle : 'Em à !' ,
+		    introTitle : ' Nhi sâu à!' ,
+		    introDesc : Thường lệ vốn trái đất
+		    Mà sao em cứ đi nhầm đường
+		    Lạc vào tim anh lẻ loi
+		    Đằng sau chữ yêu đây là thương` ,
+		    btnIntro : ' hihi ' ,
+		    title : 'Phải chăng em yêu ngay từ cái nhìn đầu tiên 😙 ' ,
+		    desc : 'Phải chăng em đã nói ngay từ khi thấy nụ cười xuất hiện' ,
+		    btnCó : ' Cứ là thích anh <33' ,
+		    btnNo : ' Không, Anh trai à: 3 ' ,
+		    question : 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3 ' ,
+		    btnReply : 'Gửi cho anh <3' ,
+		    reply : ' Yêu thì yêu mà không yêu thì yêu <33333333 ' ,
+		    lộn xộn : ' Anh biết mà 🥰. Yêu em nhiều ' ,
+		    messDesc : ' Tối nay 7h anh qua đón nhé công chúa. ' ,
+		    btn Chấp nhận : ' Okiiiii lun <3 ' ,
+		    btnIntro : ' Tiếp theo ' ,
+		    title : 'Phải chăng Nhi sâu bị khùng: >> ' ,
+		    desc : 'Phải chăng sâu Nhi bị đin :)) ' ,
+		    btn Có : ' Không nhoa <33' ,
+		    btnNo : ' Đúng dị, Nhi bị đin :)) ' ,
+		    question : 'Trên thế giới hơn 7 tỉ người mà sao Nhi lại bị đin ??? ' ,
+		    btnReply : 'Gửi cho Như bò <3' ,
+		    reply : ' Đin thì đin không có thì khùng ' ,
+		    lộn xộn : ' Biết mà : >>. Nhi deep bị khùng ' ,
+		    messDesc : ' Hoiii Nhi sâu bị khùng ngủ ngonnn ' ,
+		    btnAccept : ' Oke lun :)) ' ,
+		    messLink : 'http://fb.com'  // liên kết lộn xộn của các bạn. VD: https://m.me/nam.nodemy
+		} 
+		}
+
